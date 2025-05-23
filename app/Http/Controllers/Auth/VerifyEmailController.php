8d20d2ec8266316d585b38ded2e758869903bd5d
Junn Xavier Adalid <xaviadalid959@gmail.com>
2025-05-23 16:11:15 +0800

    last push may 23 for updated logo and disable required email verification

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -14,14 +14,7 @@ class VerifyEmailController extends Controller
      */
     public function __invoke(EmailVerificationRequest $request): RedirectResponse
     {
-        if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
-        }
-
-        if ($request->user()->markEmailAsVerified()) {
-            event(new Verified($request->user()));
-        }
-
-        return redirect()->intended(route('dashboard', absolute: false).'?verified=1');
+        // Skip verification and redirect to dashboard
+        return redirect()->intended(route('dashboard', absolute: false));
     }
 }
